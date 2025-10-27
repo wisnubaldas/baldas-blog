cde2f3356c70212e697364ff4e19f92e7f92ed4c
wisnuhidayat <wisnubaldas@gmail.com>
2025-10-27 12:14:43 +0700

    dasdasdad

---
+++ docs/stylesheets/custom.js
@@ -0,0 +1,3 @@
+import "https://code.jquery.com/jquery-3.4.1.slim.min.js";
+import "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
+import "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js";
