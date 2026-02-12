98f7530ff222dc9048ffe5e7e9d3b879e72bf997
wisnuhidayat <wisnubaldas@gmail.com>
2026-02-12 18:50:42 +0700

    start wms app

--- src/features/apps/data/apps.js
+++ src/features/apps/data/apps.js
@@ -1,11 +1,41 @@
 export const apps = [
-  { key: 'edi', name: 'EDI', description: 'Electronic Data Interchange', icon: 'solar:document-add-broken', path: '/edi' },
-  { key: 'tps-online', name: 'TPS Online', description: 'Beacukai Cargo Monitoring', icon: 'mdi:radar', path: '/tps-online' },
-  { key: 'ap-ii', name: 'AP II', description: 'Angkasapura II Invoice', icon: 'ph:air-traffic-control-duotone', path: '/ap-ii' },
-  { key: 'hubnet', name: 'HUBNET', description: 'Dinas Perhubungan Tracking', icon: 'mdi:lan', path: '/hubnet' },
-  { key: 'setting', name: 'Setting', description: 'Manage Application', icon: 'lucide:settings', path: '/setting' },
-]
+  {
+    key: "wms",
+    name: "WMS",
+    description: "Warehouse Management System",
+    icon: "solar:document-add-broken",
+    path: "https://app.wisnubaldas.net",
+  },
+  {
+    key: "tps-online",
+    name: "TPS Online",
+    description: "Beacukai Cargo Monitoring",
+    icon: "mdi:radar",
+    path: "/tps-online",
+  },
+  {
+    key: "ap-ii",
+    name: "AP II",
+    description: "Angkasapura II Invoice",
+    icon: "ph:air-traffic-control-duotone",
+    path: "/ap-ii",
+  },
+  {
+    key: "hubnet",
+    name: "HUBNET",
+    description: "Dinas Perhubungan Tracking",
+    icon: "mdi:lan",
+    path: "/hubnet",
+  },
+  {
+    key: "setting",
+    name: "Setting",
+    description: "Manage Application",
+    icon: "lucide:settings",
+    path: "/setting",
+  },
+];
 
 export function getApp(keyOrPath) {
-  return apps.find((app) => app.key === keyOrPath || app.path === keyOrPath)
+  return apps.find((app) => app.key === keyOrPath || app.path === keyOrPath);
 }
